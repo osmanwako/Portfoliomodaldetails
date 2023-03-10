ef3271286aede94a05d257011d27177bfe55a900
OsmanFP <osmanwako411@gmail.com>
2023-03-10 16:34:14 +0300

    create function for  localstroge contact me

--- assets/validation.js
+++ assets/validation.js
@@ -1,5 +1,21 @@
+const { json } = require('npm-registry-fetch');
+
 const errorlabel = document.querySelector('#errorlabelid');
+const form = document.querySelector('#contactformid');
+const name = document.querySelector('#name');
 const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+
+if (!localStorage.getItem('storedcontactme')) {
+  localStorage.setItem(
+    'contactmeinput',
+    JSON.stringify({ name: '', email: '', message: '' })
+  );
+}
+const storedcontactme = JSON.parse(localStorage.getItem('storedcontactme'));
+name.value = storedcontactme.name;
+email.value = storedcontactme.email;
+message.value = storedcontactme.message;
 
 function showerror() {
   email.style.border = '3px solid red';
@@ -15,6 +31,13 @@ function checkform(event) {
   event.preventDefault();
   return showerror();
 }
+function recordinput(event) {
+  const [input, value] = [event.target.name, event.target.value];
+  storedcontactme[input] = value;
+  localStorage.setItem('storedcontactme', JSON.stringify(storedcontactme));
+}
 
-const form = document.querySelector('#contactformid');
 form.addEventListener('submit', checkform);
+email.addEventListener('change', recordinput);
+name.addEventListener('change', recordinput);
+message.addEventListener('change', recordinput);
